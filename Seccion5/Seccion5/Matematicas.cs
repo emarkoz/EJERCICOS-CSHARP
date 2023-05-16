5f8774a9549eaeaab566153e6bb81db51636bbb9
emarkoz <20935906+emarkoz@users.noreply.github.com>
2023-05-16 12:43:08 -0700

    Add files via upload

---
+++ Seccion5/Seccion5/Matematicas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion5
+{
+    internal class Matematicas
+    {
+        private double numero1, numero2, resultado;
+
+        public void Sumar()
+        {
+            Console.WriteLine("Dame el primer numero");
+            numero1= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Dame el segundo numero");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            resultado = numero1 + numero2;
+
+            Console.WriteLine("El resultado de la suma es: {0} + {1} = {2}", numero1, numero2, resultado);
+        }
+
+        public void Restar(double num1Pa, double num2Pa) //parametros
+        {
+            
+            resultado = num1Pa - num2Pa; //operacion con parametros recibidos de argumentos en Program.cs
+            Console.WriteLine("El resultado de la resta es: {0} - {1} = {2}", num1Pa, num2Pa, resultado);
+        }
+        public void Multiplicar()
+        {
+            Console.WriteLine("Dame el primer numero");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Dame el segundo numero");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            resultado = numero1 * numero2;
+            Console.WriteLine("El resultado de la multiplicacion es: {0} * {1} = {2}", numero1, numero2, resultado);
+        }
+        public void Dividir()
+        {
+            Console.WriteLine("Dame el primer numero");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Dame el segundo numero");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            resultado = numero1 / numero2;
+            Console.WriteLine("El resultado de la division es: {0} / {1} = {2}", numero1, numero2, resultado);
+        }
+    }
+}
---
+++ Seccion5/Seccion5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematicas sumas = new Matematicas();
+            int opcion;
+            double num1Ar, num2Ar; //argumentos
+
+            Console.WriteLine("Que operacion deseas realizar?");
+            Console.WriteLine("1.- Suma");
+            Console.WriteLine("2.- Resta");
+            Console.WriteLine("3.- Multiplicacion");
+            Console.WriteLine("4.- Division");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            if (opcion == 1) {
+                sumas.Sumar();
+            } else if (opcion == 2)
+            {
+                Console.WriteLine("Dame el primer numero");
+                num1Ar = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Dame el segundo numero");
+                num2Ar = Convert.ToDouble(Console.ReadLine());
+                sumas.Restar(num1Ar, num2Ar); //pasando argumentos al metodo
+                
+            } else if (opcion == 3)
+            {
+                sumas.Multiplicar();
+            } else if (opcion == 4)
+            {
+                sumas.Dividir();
+            } else
+            {
+                Console.WriteLine("Opcion no valida");  
+            }
+            
+           
+            Console.ReadKey();
+        }
+    }
+}
